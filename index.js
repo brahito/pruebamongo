4aae0ada71806b0fd744125d19a49f996e15ca6c
Brayan Stiven Moreno Gonzalez <1151969576@academico.icesi.edu.co>
2019-04-29 08:30:30 -0500

    intento con mongo

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+var MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'tienda';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  
+  const productos = db.collection('tienda');
+    productos.find({},{ sort:['precio'] }).toArray(function(err, datos){
+        
+    });
+
+  client.close();
+});
+
+
+
+
 var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
+
+
+
 
 app.use(express.static('public'));
 
