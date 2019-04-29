edd88fcfc46d6752c2ad667b20067da524531c12
Brayan Stiven Moreno Gonzalez <1151969576@academico.icesi.edu.co>
2019-04-29 07:52:27 -0500

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(request, response){
+    var contexto = {
+        titulo: 'Página principal',
+    };
+    response.render('home', contexto);
+});
+
+app.listen(3000);
